ff2c21b7c550314ba8ed00b97e137a4587e5faa8
Doug Crawford <doug.crawford@intelight-its.com>
2016-02-09 13:24:20 -0800

    fio: update fio module and library versions to 1.2

--- fio/src/fiodriver/fiodrvr.c
+++ fio/src/fiodriver/fiodrvr.c
@@ -280,6 +280,6 @@ $Log$
 /*****************************************************************************/
 MODULE_AUTHOR( "Thomas E. Gauger tgauger@vanteon.com" );
 MODULE_DESCRIPTION( "FIO API Module for ATC" );
-MODULE_VERSION( "01.00" );
+MODULE_VERSION( "1.2" );
 MODULE_LICENSE("GPL");
 
