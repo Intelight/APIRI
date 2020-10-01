aff6922759644c319b016032b48f3e747d573669
Douglas Tarico <douglas.tarico@q-free.com>
2020-10-01 16:54:26 -0700

    fiodriver: increment driver version to 1.27

--- fio/src/fiodriver/fiodrvr.c
+++ fio/src/fiodriver/fiodrvr.c
@@ -280,6 +280,6 @@ $Log$
 /*****************************************************************************/
 MODULE_AUTHOR( "Thomas E. Gauger tgauger@vanteon.com" );
 MODULE_DESCRIPTION( "FIO API Module for ATC" );
-MODULE_VERSION( "1.26" );
+MODULE_VERSION( "1.27" );
 MODULE_LICENSE("GPL");
 
