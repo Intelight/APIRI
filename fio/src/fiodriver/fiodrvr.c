ad9f5d04333a26eacf3e9eb66402adeb13687fcf
Doug Crawford <doug.crawford@intelight-its.com>
2022-07-31 13:15:45 -0700

    fio: bump driver version to 1.30

--- fio/src/fiodriver/fiodrvr.c
+++ fio/src/fiodriver/fiodrvr.c
@@ -280,6 +280,6 @@ $Log$
 /*****************************************************************************/
 MODULE_AUTHOR( "Thomas E. Gauger tgauger@vanteon.com" );
 MODULE_DESCRIPTION( "FIO API Module for ATC" );
-MODULE_VERSION( "1.29" );
+MODULE_VERSION( "1.30" );
 MODULE_LICENSE("GPL");
 
