092de05ac662139cc5c0dba4bd8630f8659d8056
Mike Gallagher <Michael.Gallagher@q-free.com>
2022-08-10 11:20:22 -0700

    fiodriver: increase service time allowed in frame 51 deadtime calc (#13)
    * fiodriver: increase service time allowed in frame 51 deadtime calculation
    
    * fiodriver: bump driver version to 1.31

--- fio/src/fiodriver/fiodrvr.c
+++ fio/src/fiodriver/fiodrvr.c
@@ -280,6 +280,6 @@ $Log$
 /*****************************************************************************/
 MODULE_AUTHOR( "Thomas E. Gauger tgauger@vanteon.com" );
 MODULE_DESCRIPTION( "FIO API Module for ATC" );
-MODULE_VERSION( "1.30" );
+MODULE_VERSION( "1.31" );
 MODULE_LICENSE("GPL");
 
