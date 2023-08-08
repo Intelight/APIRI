c87923793f6fd74f51917c7b551512a55568518c
Mike Gallagher <mike.gallagher@intelight-its.com>
2023-08-08 09:22:21 -0700

    fio: bump driver version to v1.33

--- fio/src/fiodriver/fiodrvr.c
+++ fio/src/fiodriver/fiodrvr.c
@@ -280,6 +280,6 @@ $Log$
 /*****************************************************************************/
 MODULE_AUTHOR( "Thomas E. Gauger tgauger@vanteon.com" );
 MODULE_DESCRIPTION( "FIO API Module for ATC" );
-MODULE_VERSION( "1.32" );
+MODULE_VERSION( "1.33" );
 MODULE_LICENSE("GPL");
 
