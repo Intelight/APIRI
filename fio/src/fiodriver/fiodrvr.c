bad54dc3710380fa1511bc06e104fe3fc75bf1d8
John Wolff <John.Wolff@q-free.com>
2024-04-19 13:51:54 -0400

    bump version

--- fio/src/fiodriver/fiodrvr.c
+++ fio/src/fiodriver/fiodrvr.c
@@ -280,6 +280,6 @@ $Log$
 /*****************************************************************************/
 MODULE_AUTHOR( "Thomas E. Gauger tgauger@vanteon.com" );
 MODULE_DESCRIPTION( "FIO API Module for ATC" );
-MODULE_VERSION( "1.33" );
+MODULE_VERSION( "1.34" );
 MODULE_LICENSE("GPL");
 
