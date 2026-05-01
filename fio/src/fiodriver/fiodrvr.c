6932c2ef0c35e8665e5885e1b4ff57989104adbc
Michael Gallagher <michael.gallagher@q-free.com>
2026-05-01 17:43:33 +0100

    fiodriver: update to use kernel gpio descriptor model

--- fio/src/fiodriver/fiodrvr.c
+++ fio/src/fiodriver/fiodrvr.c
@@ -33,7 +33,7 @@
 dev_t fio_dev; /* Major / Minor */
 struct cdev fio_cdev; /* character device */
 
-int faultmon_gpio = -1;
+struct gpio_desc *faultmon_gpiod = NULL;
 
 
 static long fio_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
@@ -71,27 +71,20 @@ static const struct file_operations fio_fops = {
 
 static int fio_probe(struct platform_device *pdev)
 {
-	struct device_node *np = pdev->dev.of_node;
-	int gpio;
+	struct device *dev = &pdev->dev;
+	struct gpio_desc *gpiod;
 
-	gpio = of_get_gpio(np, 0); // NEMA controller type GPIO
-	if (gpio_is_valid(gpio)
-		&& (gpio_request_one(gpio, GPIOF_DIR_IN, "fiodriver") == 0)
-		&& !gpio_get_value(gpio)) {
+	gpiod = devm_gpiod_get(dev, "fiodriver", GPIOD_IN);
+	if (!IS_ERR(gpiod) && !gpiod_get_value(gpiod)) {
 		// NEMA controller type is TS2-1
-		faultmon_gpio = of_get_gpio(np, 1);
-		if (!gpio_is_valid(faultmon_gpio)) {
-			faultmon_gpio = -1;
-			dev_err(&pdev->dev, "faultmon gpio not valid\n");
-		} else if (gpio_request(faultmon_gpio, "fiodriver") != 0) {
-			faultmon_gpio = -1;
-			dev_err(&pdev->dev, "faultmon gpio request failed\n");
+		faultmon_gpiod = devm_gpiod_get(dev, "faultmon", GPIOD_OUT_LOW);
+		if (IS_ERR(faultmon_gpiod)) {
+			dev_err(dev, "faultmon gpio request failed\n");
 		} else {
-			gpio_direction_output(faultmon_gpio, 0); /* initially OFF */
-			if (gpio_cansleep(faultmon_gpio))
-				gpio_set_value_cansleep(faultmon_gpio, 0);
+			if (gpiod_cansleep(faultmon_gpiod))
+				gpiod_set_value_cansleep(faultmon_gpiod, 0);
 			else
-				gpio_set_value(faultmon_gpio, 0);
+				gpiod_set_value(faultmon_gpiod, 0);
 			dev_info(&pdev->dev, "faultmon gpio enabled\n");
 		}
 	}
@@ -105,8 +98,6 @@ static int fio_probe(struct platform_device *pdev)
 
 static int fio_remove(struct platform_device *pdev)
 {
-
-	gpio_free(faultmon_gpio);
 
 	dev_set_drvdata(&pdev->dev, NULL);
 
